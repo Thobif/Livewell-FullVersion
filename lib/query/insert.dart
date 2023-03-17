fcd9691ab17148cc84787811685960cfb64ee250
Thobi <jirayoot537@gmail.com>
2023-03-17 14:14:06 +0700

    first commit

--- lib/query/insert.dart
+++ lib/query/insert.dart
@@ -1,54 +1,68 @@
-
-import 'dart:ffi';
-
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Insert extends StatefulWidget {
-  const Insert({super.key});
- 
   @override
-  State<Insert> createState() => _InsertState();
+  _InsertState createState() => _InsertState();
 }
 
 class _InsertState extends State<Insert> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final nameController = TextEditingController();
   final ageController = TextEditingController();
-  @override
-  void initState() {
-    super.initState();
-     
-  }
-  void _addUserDataToFirestore(String name, double age) async {
-  await _firestore.collection('user').add({'name': name , 'age': age });
-}
 
+  void _addUserDataToFirestore(String name, int age) async {
+    await _firestore.collection('user').add({'name': name, 'age': age});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-           TextField(
-            controller: nameController,
-            decoration: InputDecoration(label: Text("Name")),
-          ),
-          TextField(
-            controller: ageController,
-            decoration: InputDecoration(label: Text("Age")),
-            keyboardType: TextInputType.numberWithOptions(decimal: true), // เพิ่ม keyboardType เป็น number และกำหนดให้รองรับ decimal
-          ),
-           ElevatedButton(
-  onPressed: () {
-              _addUserDataToFirestore(nameController.text, double.parse(ageController.text)); // แปลง String เป็น double ด้วย double.parse()
-            },
-  child: Text('Add'),
-)
-        ],
-      )),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: "Name"),
+            ),
+            TextField(
+              controller: ageController,
+              decoration: InputDecoration(labelText: "Age"),
+              keyboardType:
+                  TextInputType.numberWithOptions(decimal: true), // เพิ่ม keyboardType เป็น number และกำหนดให้รองรับ decimal
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _addUserDataToFirestore(
+                    nameController.text, int.parse(ageController.text)); // แปลง String เป็น double ด้วย double.parse()
+              },
+              child: Text('Add'),
+            ),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _firestore.collection('user').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  final documents = snapshot.data!.docs;
+                  return ListView.builder(
+                    itemCount: documents.length,
+                    itemBuilder: (context, index) {
+                      final name = documents[index]['name'];
+                      final age = documents[index]['age'];
+                      return ListTile(
+                        title: Text(name),
+                        subtitle: Text(age.toString()),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
